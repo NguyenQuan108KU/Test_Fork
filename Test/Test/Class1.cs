5f59b4c6d5562967fbabb93001446ae019da0087
Nguyễn Đức Ngọc Quân <nguyenducngocquan2004@gmail.com>
2025-11-05 01:17:01 +0700

    đây la nhanh abc

---
+++ Test/Test/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin chào! Đây là hàm Main đang chạy.");
+            Console.ReadKey(); // Giữ cửa sổ console không tắt ngay
+        }
+    }
+}
